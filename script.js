43423c34a291d149e31ec3c58bbdf6a3b14ecafb
Marvin Ren <marvin.ren@yale.edu>
2021-08-29 15:39:00 -0400

    fix bug

--- script.js
+++ script.js
@@ -1,52 +1,46 @@
-// Create an array of the special character choices
+//Array of the character choices and selected array
 var specialCharacter = [' ', '!', '"', '#', '$', '%', '&', ',', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '^', '`', '{', '|', '}', '~'];
 var numericCharacter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCaseCharacter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'o', 'w', 'x', 'y', 'z'];
 var upperCaseCharacter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'O', 'W', 'X', 'Y', 'Z'];
-var outPut = [];
+var allChars = [];
+
 
 // Assignment code here
 var generatePassword = function() {
   var characterAmount = window.prompt("How many characters would you like your password to contain?");
     if (characterAmount >= 8 && characterAmount <= 128) {
-      console.log(characterAmount)
     } else {
       window.alert("Please enter a number between 8 and 128")
+      generatePassword();
     }
   var specialConfirm = confirm("Click OK to confirm including special characters.")
   var numericConfirm = confirm("Click OK to confirm including numeric characters.")
   var lowerCaseConfirm = confirm("Click OK to confirm including lowercase characters.")
   var upperCaseConfirm = confirm("Click OK to confirm including uppercase characters.")
+    generate(specialConfirm, numericConfirm, lowerCaseConfirm, upperCaseConfirm);
+
+    for (var i = 0; i < characterAmount; i++) {
+      var value= allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
 }
 
 
-
-
-
-// generate password
-var generate = function () {
-  for (var i = 0; i < characterAmount; i++) {
-    if (specialConfirm) { 
-    specialCharacter = specialChatacter[Math.floor(Math.random()*specialCharacter.length)];
-    outPut.push(SpecialCharacter);
-    };
-
-
-    if (numericConfirm) {
-    ranNumericCharacter = numericCharacter[Math.floor(Math.random()*numericCharacter.length)];
-    outPut.push(ranNumericCharacter);
-    };
-
-    if (lowerCaseConfirm) {
-    ranLowerCaseCharacter = lowerCaseCharacter[Math.floor(Math.random()*lowerCaseCharacter.length)];
-    outPut.push(ranlowerCaseCharacter);
-    };
-
-    if (upperCaseConfirm) { 
-    ranUpperCaseCharacter = upperCaseCharacter[Math.floor(Math.random() * upperCaseCharacter.length)];
-    outPut.push(ranUpperCaseCharacter);
-    };
-  }
+// function to generate password
+var generate = function (specialConfirm, numericConfirm, lowerCaseConfirm, upperCaseConfirm) {
+  if (specialConfirm) { 
+   allChars.push(specialCharacter)
+  };
+  if (numericConfirm) {
+   allChars.push(numericCharacter);
+  };
+  if (lowerCaseConfirm) {
+   allChars.push(lowerCaseCharacter);
+  };
+  if (upperCaseConfirm) { 
+   allChars.push(upperCaseCharacter);
+  };
 };
 
 
@@ -63,3 +57,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//specialCharacter = specialChatacter[Math.floor(Math.random()*specialCharacter.length)];
